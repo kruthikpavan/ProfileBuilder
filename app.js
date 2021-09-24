954c6bd995b8bcaf33328ff74d55cc17619c614f
kruthikpavan <kruthikpavan24@gmail.com>
2021-09-24 22:52:53 +0530

    day1

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+// middleware
+app.use(express.static('public'));
+
+// view engine
+app.set('view engine', 'ejs');
+
+// database connection
+const dbURI = 'mongodb+srv://pavan:pavan@cluster0.6elbj.mongodb.net/PROFILEBUILDER?retryWrites=true&w=majority';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true})
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
+
+// routes
+app.get('/', (req, res) => res.render('home'));
+app.get('/profile', (req, res) => res.render('profile'));
