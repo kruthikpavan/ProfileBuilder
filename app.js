93cb7d8f48130abd1c3f4b8377ab4914ffdaef0e
kruthikpavan <kruthikpavan24@gmail.com>
2021-09-24 23:25:13 +0530

    day 1

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+require("dotenv/config")
 
 const app = express();
 
@@ -10,11 +11,25 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 // database connection
-const dbURI = 'mongodb+srv://pavan:pavan@cluster0.6elbj.mongodb.net/PROFILEBUILDER?retryWrites=true&w=majority';
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true})
-  .then((result) => app.listen(3000))
-  .catch((err) => console.log(err));
 
-// routes
-app.get('/', (req, res) => res.render('home'));
-app.get('/profile', (req, res) => res.render('profile'));
+
+
+  mongoose.connect(process.env.DB_CONNECTION,()=>{
+    console.log("connected to db");
+})
+
+//import  routes
+
+const Routes= require('./routes/profile');
+app.use("/",Routes)
+
+
+
+
+
+//listen
+app.listen(3000,()=>{
+    console.log("listening to server");
+})
+
+
